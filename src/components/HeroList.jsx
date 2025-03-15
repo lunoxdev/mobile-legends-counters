98039c40e48f3dfcfef8a0de1538f7872a86bfcd
lunoxdev <lunox.code@gmail.com>
2025-03-15 12:11:17 -0600

    improve styles and add rotate phone message

--- src/components/HeroList.jsx
+++ src/components/HeroList.jsx
@@ -2,41 +2,25 @@ import { useEffect, useState } from "react";
 import { Card, CardFooter, Image, Button } from "@nextui-org/react";
 
 export default function HeroList() {
-  const [roles, setRoles] = useState([]);
   const [heroes, setHeroes] = useState([]);
   const [selectedRoleId, setSelectedRoleId] = useState(1);
   const [activeRoleId, setActiveRoleId] = useState(1);
 
-  // Fetch roles
-  useEffect(() => {
-    const fetchRoles = async () => {
-      const url = "https://unofficial-mobile-legends.p.rapidapi.com/roles";
-      const options = {
-        method: "GET",
-        headers: {
-          "x-rapidapi-key": import.meta.env.PUBLIC_RAPIDAPI_KEY,
-          "x-rapidapi-host": import.meta.env.PUBLIC_RAPIDAPI_HOST,
-        },
-      };
+  const roles = [
+    { id: 1, name: "ALL" },
+    { id: "Tank", name: "Tank" },
+    { id: "Fighter", name: "Fighter" },
+    { id: "Assassin", name: "Assassin" },
+    { id: "Mage", name: "Mage" },
+    { id: "Marksman", name: "Marksman" },
+    { id: "Support", name: "Support" },
+  ];
 
-      try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        setRoles(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchRoles();
-  }, []);
-
-  // Fetch heroes
   useEffect(() => {
     const fetchHeroes = async () => {
       if (!selectedRoleId) return;
 
-      const url = `https://unofficial-mobile-legends.p.rapidapi.com/roles/${selectedRoleId}`; // Asegúrate de que esta URL sea correcta
+      const url = `https://unofficial-mobile-legends.p.rapidapi.com/roles/${selectedRoleId}`;
       const options = {
         method: "GET",
         headers: {
@@ -57,54 +41,41 @@ export default function HeroList() {
     fetchHeroes();
   }, [selectedRoleId]);
 
+  const handleRoleClick = (roleId) => {
+    setSelectedRoleId(roleId);
+    setActiveRoleId(roleId);
+  };
+
   return (
-    <section className="w-full my-5">
-      {/* Bar */}
-      <nav className="flex justify-center items-center gap-2 mb-5">
-        <Button
-          onClick={() => {
-            setSelectedRoleId(1);
-            setActiveRoleId(1);
-          }}
-          className={`hover:bg-blue-500 rounded-md px-5 py-2 ${
-            activeRoleId === 1 ? "bg-blue-500" : ""
-          }`}
-        >
-          ALL
-        </Button>
+    <section className="w-full h-screen px-2">
+      <nav className="flex shrink-0">
         {roles.map((role) => (
           <Button
             key={role.id}
-            onClick={() => {
-              setSelectedRoleId(role.label);
-              setActiveRoleId(role.label);
-            }}
-            className={`hover:bg-blue-500 rounded-md px-5 py-2 ${
-              activeRoleId === role.label ? "bg-blue-500" : ""
+            onClick={() => handleRoleClick(role.id)}
+            className={`px-5 py-3 border-b-[0.1px] outline-none border-[#7890B3] text-[#7890B3] hover:text-[#f2f2f2] hover:bg-gradient-to-t hover:from-[#5C67B8] hover:to-[#5C67B8]/5 ${
+              activeRoleId === role.id ? "border-b-2 border-[#98FFFF] bg-gradient-to-t from-[#5C67B8] to-[#5C67B8]/5 text-[#f2f2f2]" : ""
             }`}
           >
-            {role.label}
+            {role.name}
           </Button>
         ))}
       </nav>
 
-      <hr />
-
-      {/* Heroes List */}
-      <Card isFooterBlurred radius="lg" className="grid grid-cols-8 gap-2 mt-5">
+      <Card className="grid grid-cols-7 lg:grid-cols-10 h-auto gap-3 py-4">
         {heroes.map((hero) => (
           <div
             key={hero.heroid}
-            className="relative overflow-hidden rounded-lg border-none hover:shadow-md hover:shadow-blue-500 my-1"
+            className="relative overflow-hidden h-auto rounded-lg hover:shadow-md hover:shadow-blue-500"
           >
             <Image
               src={hero.key}
               alt={hero.name}
-              height={220}
-              width={200}
+              height={170}
+              width={160}
               className="object-cover transition-transform duration-300 ease-in-out hover:scale-110"
             />
-            <CardFooter className="absolute bottom-0 left-0 right-0 z-10 bg-gradient-to-t from-blue-900/80 via-blue-950/40 to-transparent font-bold px-2 pt-14 text-lg justify-center">
+            <CardFooter className="absolute bottom-0 left-0 right-0 z-10 bg-gradient-to-t from-[#292E52] via-[#292E52]/70 to-transparent px-2 pt-10 text-sm justify-center">
               {hero.name}
             </CardFooter>
           </div>
